9f6a8cb8be4d2af6b28101d79ee55a2f45e06ab5
desser2002 <oxxxymironushe@gmail.com>
2025-04-17 19:41:04 +0200

    stream 2

--- src/main/java/domen/Main.java
+++ src/main/java/domen/Main.java
@@ -43,5 +43,136 @@ public class Main {
         //  MyStream<String> stream = new MyStream<>(names);
         //  System.out.print(stream.filter(name -> name.startsWith("A")).map(String::toUpperCase).count());
 
+//       Task 1:
+//        List<Integer> numbers = Arrays.asList(2, 4, 6, 8);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        boolean allEven = stream.allMatch(n -> n % 2 == 0);
+//        System.out.println(allEven);  // Wyjście: true
+
+//       Task 2:
+//        List<Integer> numbers = Arrays.asList(1, 3, 4, 5);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        boolean hasEven = stream.anyMatch(n -> n % 2 == 0);
+//        System.out.println(hasEven);  // Wyjście: true
+
+//       Task 3:
+//        List<Integer> numbers = Arrays.asList(2, 4, 6, 9, 12);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        System.out.println("allMatch: " + stream.allMatch(x -> x%2 == 0));
+//        System.out.println("anyMatch: " + stream.anyMatch(x -> x>10));
+
+//       Task 4:
+//        List<Integer> numbers = Arrays.asList(1, 3, 5, 7);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        boolean noneEven = stream.noneMatch(n -> n % 2 == 0);
+//        System.out.println(noneEven);  // Wyjście: true
+
+//       Task 5:
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        int sum = stream.reduce(0, Integer::sum);
+//        System.out.println(sum);  // Wyjście: 10
+
+//        Task 6:
+//        List<Integer> numbers = Arrays.asList(12, 15, 8, 22);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//
+//        System.out.println("Sum: " + stream.filter(n -> n > 10).reduce(0, Integer::sum));
+//        System.out.println("Wynik noneMatch:: " + stream.noneMatch(n -> n < 5));
+
+//        Task 7:
+//        List<Integer> numbers = Arrays.asList(4, 2, 6, 1);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        Optional<Integer> minValue = stream.min(Integer::compareTo);
+//        minValue.ifPresent(System.out::println);  // Wyjście: 1
+//
+//        Task 8:
+//        List<Integer> numbers = Arrays.asList(4, 2, 6, 1);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//        Optional<Integer> maxValue = stream.max(Integer::compareTo);
+//        maxValue.ifPresent(System.out::println);  // Wyjście: 6
+
+//        Task 9:
+//        List<Integer> numbers = Arrays.asList(4, 2, 6, 1);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//
+//        Optional<Integer> minValue = stream.min(Integer::compareTo);
+//        minValue.ifPresent(min -> System.out.println("Minimalna: " + min));
+//
+//        Optional<Integer> maxValue = stream.max(Integer::compareTo);
+//        maxValue.ifPresent(max -> System.out.println("Maximalna: " + max));
+//
+//        int sum = stream.reduce(0, Integer::sum);
+//        System.out.println("Suma: " + sum);
+
+//        Task 10:
+//        List<String> names = Arrays.asList("Anna", "Bartek", "Cecylia");
+//        MyStream<String> stream = new MyStream<>(names);
+//        stream.peek(System.out::println).toList();
+
+//// Wyjście:
+//// Anna
+//// Bartek
+//// Cecylia
+
+//        Task 11:
+//        List<List<Integer>> numbers = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3, 4));
+//        MyStream<List<Integer>> stream = new MyStream<>(numbers);
+//        MyStream<Integer> flattened = stream.flatMap(list -> new MyStream<>(list));
+//        List<Integer> result = flattened.toList();
+//        System.out.println(result);  // Wyjście: [1, 2, 3, 4]
+
+//        Task 12:
+//        List<List<Integer>> numbers = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3, 4), Arrays.asList(5, 6));
+//        MyStream<List<Integer>> stream = new MyStream<>(numbers);
+//        MyStream <Integer> flattend = stream.flatMap(list-> new MyStream<>(list)).peek(element -> System.out.print(element+ " "));
+//
+
+//        Task 13:
+//        List<String> names = Arrays.asList("Anna", "Bartek", "Cecylia");
+//        MyStream<String> stream = new MyStream<>(names);
+//        String result = stream.join(", ");
+//        System.out.println(result);  // Wyjście: "Anna, Bartek, Cecylia"
+//
+
+//      Task 14.1:
+//        List<Integer> numbers = Arrays.asList(12, 50, 70, 33);
+//        MyStream<Integer> stream = new MyStream<>(numbers);
+//
+//        System.out.println("allMatch :" + stream.allMatch(n -> n > 0));
+//        System.out.println("noneMatch:" + stream.noneMatch(n -> n > 100));
+
+//       Task 14.2:
+//        List<String> names = Arrays.asList("Anna", "Agnieszka", "Bartek", "Cecylia", "Adam");
+//        MyStream<String> stream = new MyStream<>(names);
+//
+//        System.out.print(stream.filter(name -> name.startsWith("A")).map(String::toUpperCase).join(", "));
+
+//        Task 14.3:
+//        List <Integer> list = Arrays.asList(10,15,20,10);
+//        MyStream <Integer> stream = new MyStream<>(list);
+//
+//        System.out.println("Suma: " + stream.reduce(0,Integer::sum));
+//        System.out.println("Wynik noneMatch: " + stream.noneMatch(n->n>50));
+
+//       Task 14.4:
+//        List<Integer> list = Arrays.asList(3, 7, 9, 2, 8);
+//
+//        MyStream<Integer> stream = new MyStream<>(list);
+//
+//        Optional<Integer> minValue = stream.min(Integer::compareTo);
+//        minValue.ifPresent(min -> System.out.println("Minimalna: " + min));
+//
+//        Optional<Integer> maxValue = stream.max(Integer::compareTo);
+//        maxValue.ifPresent(max -> System.out.println("Maximalna: " + max));
+
+//        Test 14.5:
+//        List<List<Integer>> numbers = Arrays.asList(Arrays.asList(1,2), Arrays.asList(3,4), Arrays.asList(5,6));
+//        MyStream <List<Integer>> stream = new MyStream<>(numbers);
+//
+//        System.out.print("Drukowane elementy: ");
+//        MyStream<Integer> flatten =stream.flatMap(list->new MyStream<>(list)).peek(n->System.out.print(n+" "));
+//        System.out.print("\nSuma: "+ flatten.reduce(0,Integer::sum));
+
     }
 }
--- src/main/java/domen/MyStream.java
+++ src/main/java/domen/MyStream.java
@@ -1,6 +1,7 @@
 package domen;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -26,13 +27,11 @@ public class MyStream<T> {
     }
 
     public <R> MyStream<R> map(Function<T, R> mapper) {
-        ArrayList<R> result = new ArrayList<>();
+        ArrayList<R> result = new ArrayList<>(elements.size());
         for (T element : elements) {
             result.add(mapper.apply(element));
         }
         return new MyStream<>(result);
-
-
     }
 
     public void forEach(Consumer<T> action) {
@@ -50,16 +49,19 @@ public class MyStream<T> {
     }
 
     public MyStream<T> limit(int maxSize) {
-        ArrayList<T> limit = new ArrayList<>();
-        for (T element : elements) {
-            if (limit.size() < maxSize)
-                limit.add(element);
+
+        boolean shouldLimit = maxSize > elements.size();
+        ArrayList<T> limited = new ArrayList<>(shouldLimit ? elements.size() : maxSize);
+
+        for (int i = 0; i < (shouldLimit ? elements.size() : maxSize); i++) {
+            limited.add(elements.get(i));
         }
-        return new MyStream<>(limit);
+
+        return new MyStream<>(limited);
     }
 
     public MyStream<T> skip(int n) {
-        ArrayList<T> skipped = new ArrayList<>();
+        ArrayList<T> skipped = new ArrayList<>(elements.size() - n);
         for (int k = n; k < elements.size(); k++) {
             skipped.add(elements.get(k));
         }
@@ -75,4 +77,87 @@ public class MyStream<T> {
     public Optional<T> findFirst() {
         return elements.isEmpty() ? Optional.empty() : Optional.of(elements.get(0));
     }
+
+    public boolean allMatch(Predicate<T> predicate) {
+        for (T element : elements) {
+            if (!predicate.test(element)) return false;
+        }
+        return true;
+    }
+
+    public boolean anyMatch(Predicate<T> predicate) {
+        for (T element : elements) {
+            if (predicate.test(element)) return true;
+        }
+        return false;
+    }
+
+    public boolean noneMatch(Predicate<T> predicate) {
+        for (T element : elements) {
+            if (predicate.test(element)) return false;
+        }
+        return true;
+    }
+
+    public T reduce(T identity, BinaryOperator<T> accumulator) {
+
+        for (T element : elements) {
+            identity = accumulator.apply(identity, element);
+        }
+        return identity;
+
+    }
+
+    public Optional<T> min(Comparator<T> comparator) {
+        if (elements.isEmpty()) return Optional.empty();
+
+        T min = elements.get(0);
+        for (T element : elements) {
+            if (comparator.compare(element, min) < 0) min = element;
+        }
+        return Optional.of(min);
+    }
+
+    public Optional<T> max(Comparator<T> comparator) {
+        if (elements.isEmpty()) return Optional.empty();
+
+        T max = elements.get(0);
+        for (T element : elements) {
+            if (comparator.compare(element, max) > 0) max = element;
+        }
+        return Optional.of(max);
+    }
+
+    public MyStream<T> peek(Consumer<T> action) {
+        for (T element : elements) {
+            action.accept(element);
+        }
+        return this;
+    }
+
+    public <R> MyStream<R> flatMap(Function<T, MyStream<R>> mapper) {
+        List<R> flattened = new ArrayList<>();
+
+        for (T element : elements) {
+            MyStream<R> stream = mapper.apply(element);
+            flattened.addAll(stream.toList());
+        }
+        return new MyStream<>(flattened);
+
+    }
+
+    public String join(CharSequence delimiter) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < elements.size(); i++) {
+            result.append(elements.get(i));
+
+            if (i < elements.size() - 1) {
+                result.append(delimiter);
+            }
+        }
+        return result.toString();
+    }
+
+
 }
